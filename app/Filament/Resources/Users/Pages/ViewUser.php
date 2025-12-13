1b7b21a3662f1f828b5b4ec238a2daed37209628
Ahmad Hassan <s2016-112@bnu.edu.pk>
2025-12-14 01:13:46 +0500

    Fix: Restore ViewUser.php class definition

--- app/Filament/Resources/Users/Pages/ViewUser.php
+++ app/Filament/Resources/Users/Pages/ViewUser.php
@@ -3,6 +3,22 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewUser extends ViewRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make(),
+            DeleteAction::make(),
+        ];
+    }
+    
     protected function getHeaderWidgets(): array
     {
         return [
