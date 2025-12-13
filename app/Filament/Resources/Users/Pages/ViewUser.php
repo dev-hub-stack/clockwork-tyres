c8844c91f4dfb64e3ff340edca090ef32487f752
Ahmad Hassan <s2016-112@bnu.edu.pk>
2025-12-14 01:12:17 +0500

    Fix: Remove missing LoginHistoryWidget from ViewUser page

--- app/Filament/Resources/Users/Pages/ViewUser.php
+++ app/Filament/Resources/Users/Pages/ViewUser.php
@@ -3,27 +3,10 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
-use App\Livewire\Users\LoginHistoryWidget;
-use Filament\Actions\DeleteAction;
-use Filament\Actions\EditAction;
-use Filament\Resources\Pages\ViewRecord;
-
-class ViewUser extends ViewRecord
-{
-    protected static string $resource = UserResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            EditAction::make(),
-            DeleteAction::make(),
-        ];
-    }
-    
     protected function getHeaderWidgets(): array
     {
         return [
-            LoginHistoryWidget::class,
+            //
         ];
     }
 }
