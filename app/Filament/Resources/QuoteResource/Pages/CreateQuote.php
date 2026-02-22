b34ee15d7d41e21565bf18569e44a6710ec0cc59
Ahmad Hassan <s2016-112@bnu.edu.pk>
2026-02-22 15:10:30 +0500

    fix(quotes): add missing closing brace in CreateQuote class

--- app/Filament/Resources/QuoteResource/Pages/CreateQuote.php
+++ app/Filament/Resources/QuoteResource/Pages/CreateQuote.php
@@ -143,4 +143,4 @@ class CreateQuote extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
-    }
+    }}
